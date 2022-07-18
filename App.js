1862d42c8ac6aea122f0f32b249b3ec5697b5998
Talbi Achref <achref.talbi@esprit.tn>
2022-07-18 14:09:40 +0100

    Home_screen

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './screens/HomeScreen';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './screens/MapScreen';
+
+
+
+
 
 export default function App() {
+
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+
+            <Stack.Screen
+              name='HomeScreen'
+              component={HomeScreen}
+              options ={{
+               headerShown:false,
+              }}
+            />
+            <Stack.Screen
+              name='MapScreen'
+              component={MapScreen}
+              options ={{
+               headerShown:false,
+              }}
+            />
+          </Stack.Navigator>
+
+
+
+
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
